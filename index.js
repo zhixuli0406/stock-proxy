84430af3eb5634f975e396881f07e366205a3594
zhixu <zhixuli0406@gmail.com>
2023-05-16 13:35:27 +0800

    fix:  - delete port

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 // Create Express Server
 const app = express();
 
-// Configuration
-const PORT = 8080;
-const HOST = "localhost";
 const API_SERVICE_URL = "https://tw.stock.yahoo.com";
 
 // Proxy endpoints
@@ -14,8 +11,3 @@ app.use('/_td-stock/api', createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
 }));
-
-// Start Proxy
-app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
